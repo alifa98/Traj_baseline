099747015d1c3a18bf93ebbff9773ccd4388c0dd
Ali Faraji <billa.svpc@gmail.com>
2023-07-26 23:48:57 -0400

    feat: data convertor to .dyna

---
+++ data_convert.py
@@ -0,0 +1,30 @@
+# this script converts higher order hexagon data to the format of the Libcity model
+# Output is a dyna file with the following format:
+# dyna_id,type,time,entity_id,location
+
+import os
+import pandas as pd
+import tqdm
+
+input_file = 'geolife/ho_geolife_res7.csv'
+dataset_name = 'geolife_res7'
+user_id_column = 'user_id'
+
+
+output_folder = 'raw_data/{}/'.format(dataset_name)
+output_file = output_folder + '{}.dyna'.format(dataset_name)
+
+os.makedirs(output_folder, exist_ok=True)
+
+# read input file
+input_file = pd.read_csv(input_file)
+
+output_columns = ['dyna_id', 'type', 'time', 'entity_id', 'location']
+record_id = 0
+with open(output_file,'w') as file:
+    file.write(','.join(output_columns) + '\n')
+    for index, row in tqdm.tqdm(input_file.iterrows(), desc="Converting data"):
+        for time_step, loc_hex in enumerate(row['higher_order_trajectory'].split(' ')):
+            file.write(f'{record_id},trajectory,{time_step},{row[user_id_column]},{loc_hex}\n')
+            record_id += 1
+        file.flush()
