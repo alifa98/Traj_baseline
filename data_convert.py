d53d1eed95447114378ca8f94da5ba3a8af780e7
Ali Faraji <billa.svpc@gmail.com>
2023-07-28 05:40:17 -0400

    get data about time embedding size in data convertor

--- data_convert.py
+++ data_convert.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 import tqdm
 
-input_file = 'geolife/ho_geolife_res7.csv'
+input_file = input_file = '/local/data1/shared_data/higher_order_trajectory/geolife/ho_geolife_res7.csv'
 dataset_name = 'geolife_res7'
 user_id_column = 'user_id'
 
@@ -21,10 +21,16 @@ input_file = pd.read_csv(input_file)
 
 output_columns = ['dyna_id', 'type', 'time', 'entity_id', 'location']
 record_id = 0
-with open(output_file,'w') as file:
+max_time = 0
+with open(output_file, 'w') as file:
     file.write(','.join(output_columns) + '\n')
     for index, row in tqdm.tqdm(input_file.iterrows(), desc="Converting data"):
         for time_step, loc_hex in enumerate(row['higher_order_trajectory'].split(' ')):
-            file.write(f'{record_id},trajectory,{time_step},{row[user_id_column]},{loc_hex}\n')
+            file.write(
+                f'{record_id},trajectory,{time_step},{row[user_id_column]},{loc_hex}\n')
             record_id += 1
+            max_time = max(max_time, time_step)
         file.flush()
+
+with open(f"{output_folder}{dataset_name}-info.txt", "w") as f:
+    f.write(f"max_time:{max_time}\n")
